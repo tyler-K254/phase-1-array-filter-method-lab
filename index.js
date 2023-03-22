508897abcd1c40d636bc0959b0390d8b4354fa2f
Tyler Kirumba <Tyler>
2023-03-22 20:30:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => {
+      
+      const lowercaseDriverName = driver.toLowerCase();
+      const lowercaseSearchString = string.toLowerCase();
+  
+      
+      return lowercaseDriverName.includes(lowercaseSearchString);
+    });
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
